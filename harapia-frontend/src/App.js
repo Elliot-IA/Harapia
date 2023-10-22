723c2fa1e6f61807b966731ea126edaeeac747e0
Elliot_IA <alexa818@umn.edu>
2023-10-21 20:20:31 -0500

    hey

---
+++ harapia-frontend/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import Tile from './components/tile.jsx';
+import Backdrop from './components/backdrop.jsx';
+
+function App() {
+    return (
+        <div id="supremeWrapper">
+            <Backdrop></Backdrop>
+        
+            <div id="tileContainer">
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+                <Tile></Tile>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ harapia-frontend/src/components/backdrop.jsx
@@ -0,0 +1,26 @@
+import './Backdrop.css';
+import backdrop from "../images/backdrop.webp";
+
+const Backdrop = ()=>{
+  return (
+  <div id="backdropContainer">
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+    <img className="backdrop" src={backdrop}></img>
+  </div>
+  )
+}
+
+export default Backdrop
